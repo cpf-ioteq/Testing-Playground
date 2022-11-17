df8166672e1fcf99acf9899bfa91bd75313e2870
Christopher Farish <christopher.farish@io-teq.com>
2022-11-17 14:28:53 -0600

    baseline stream, single play, and adjustable with csv are done. adjustable with csv has pauses between waves-may be a library constrant or python is too slow

---
+++ audio/Adjustable-Duration/csv_fed_duration_stream.py
@@ -0,0 +1,48 @@
+# !/usr/bin/env python3
+
+import time
+import csv
+
+import numpy as np
+import sounddevice as sd
+
+def frequency_Controller(chz, fmhz, kp):
+    # carrier_hz: Frequency of the carrier. BASE - 440.0
+    # fm_hz: Frequency of the frequency modulator. BASE - 220.0
+    # k_p: deviation constant. BASE - 25.0
+
+    sample_rate = 44100
+    carrier_hz = chz
+    fm_hz = fmhz
+    k = kp
+
+    # First, define our range of sample numbers
+    each_sample_number = np.arange(sample_rate)
+
+    # Create the carrier
+    carrier = 2 * np.pi * each_sample_number * carrier_hz / sample_rate
+
+    # Frequency modulator
+    modulator = k * np.sin(2 * np.pi * each_sample_number * fm_hz / sample_rate)
+
+    # Modulated waveform, and attenuate it
+    waveform = np.cos(carrier + modulator)
+    waveform_quiet = waveform * 0.3
+
+    # Play the waveform 
+    sd.play(waveform_quiet, sample_rate)
+    time.sleep(1)
+    sd.stop()
+
+
+filename = 'wavelength_parser.csv'
+fields = []
+rows = []
+
+with open(filename, 'r') as csvfile:
+    data_reader = csv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC)
+    for row in data_reader:
+        rows = row
+        arg1, arg2, arg3 = rows
+        print(arg1, arg2, arg3)
+        frequency_Controller(arg1, arg2, arg3)
---
+++ audio/Adjustable-Input/adjustable_input_stream.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import pyaudio
+import threading
+import numpy as np
+
+class WavePlayerLoop(threading.Thread) :
+
+  def __init__(self, freq=440., length=1., volume=0.5):
+    threading.Thread.__init__(self)
+    self.p = pyaudio.PyAudio()
+
+    self.volume = volume     # range [0.0, 1.0]
+    self.fs = 44100          # sampling rate, Hz, must be integer
+    self.duration = length   # in seconds, may be float
+    self.f = freq            # sine frequency, Hz, may be float
+
+  def run(self) :
+    # generate samples, note conversion to float32 array
+    self.samples = (np.sin(2*np.pi*np.arange(self.fs*self.duration)*self.f/self.fs)).astype(np.float32)
+
+    # for paFloat32 sample values must be in range [-1.0, 1.0]
+    self.stream = self.p.open(format=pyaudio.paFloat32,
+                    channels=1,
+                    rate=self.fs,
+                    output=True)
+
+    # play. May repeat with different volume values (if done interactively) 
+    self.stream.write(self.volume*self.samples)
+
+    self.stream.stop_stream()
+    self.stream.close()
+
+    self.p.terminate()
+
+s = WavePlayerLoop(freq=240., length=10, volume=0.5)
+r = WavePlayerLoop(freq=3000., length=10, volume=0.5)
+
+s.start()
+r.start()
